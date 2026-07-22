e2ef588f7b7d2657be5ba5aa89d21c82ecd1c7eb
Nardeen Hishmat <114638963+Nardeen-Hishmat@users.noreply.github.com>
2026-07-23 01:44:36 +0300

    Enhance Kubernetes install script with user permissions
    Updated Kubernetes installation script to improve configuration and user permissions. Added support for devsecops user and adjusted Docker settings.

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -6,25 +6,32 @@ echo "PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m
 sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
 source ~/.bashrc
 
-# Don't ask to restart services after apt update, just do it.
+# Don't ask to restart services after apt update
 [ -f /etc/needrestart/needrestart.conf ] && sed -i 's/#\$nrconf{restart} = \x27i\x27/$nrconf{restart} = \x27a\x27/' /etc/needrestart/needrestart.conf
 
-apt-get autoremove -y  #removes the packages that are no longer needed
+apt-get autoremove -y 
 apt-get update
 systemctl daemon-reload
 
+# Enable IPv4 routing (Required for Kubernetes and Flannel CNI)
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+EOF
+sudo sysctl --system
+
+# Kubernetes Installation
 KUBE_LATEST=$(curl -L -s https://dl.k8s.io/release/stable.txt | awk 'BEGIN { FS="." } { printf "%s.%s", $1, $2 }')
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/${KUBE_LATEST}/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBE_LATEST}/deb/ /" >> /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/${KUBE_LATEST}/deb/Release.key | gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBE_LATEST}/deb/ /" > /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update
-KUBE_VERSION=$(apt-cache madison kubeadm | head -1 | awk '{print $3}')
-apt-get install -y docker.io vim build-essential jq python3-pip kubelet kubectl kubernetes-cni kubeadm containerd
-pip3 install jc
+apt-get install -y docker.io vim build-essential jq jc kubelet kubectl kubeadm containerd
 
 ### UUID of VM
-### comment below line if this Script is not executed on Cloud based VMs
 jc dmidecode | jq .[1].values.uuid -r
 
 systemctl enable kubelet
@@ -37,15 +44,20 @@ mkdir -p /etc/containerd
 containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/' > /etc/containerd/config.toml
 systemctl restart containerd
 
-# uncomment below line if your host doesnt have minimum requirement of 2 CPU
-# kubeadm init --pod-network-cidr '10.244.0.0/16' --service-cidr '10.96.0.0/16' --ignore-preflight-errors=NumCPU --skip-token-print
-kubeadm init --pod-network-cidr '10.244.0.0/16' --service-cidr '10.96.0.0/16'  --skip-token-print
+# Initialize Kubernetes
+kubeadm init --pod-network-cidr '10.244.0.0/16' --service-cidr '10.96.0.0/16' --skip-token-print
 
+# 1. إعطاء صلاحيات الكوبرنيتس لليوزر root
 mkdir -p ~/.kube
 cp -i /etc/kubernetes/admin.conf ~/.kube/config
 
-kubectl apply -f "https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s-1.11.yaml"
-kubectl rollout status daemonset weave-net -n kube-system --timeout=90s
+# 2. إعطاء صلاحيات الكوبرنيتس لليوزر devsecops (تعديل جديد)
+mkdir -p /home/devsecops/.kube
+cp -i /etc/kubernetes/admin.conf /home/devsecops/.kube/config
+chown -R devsecops:devsecops /home/devsecops/.kube
+
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+
 sleep 5
 
 echo "untaint controlplane node"
@@ -58,11 +70,14 @@ kubectl get nodes -o wide
 
 echo ".........----------------#################._.-.-Docker-.-._.#################----------------........."
 
+# 3. توجيه Docker لاستخدام الهارد الإضافي لتوفير مساحة الـ OS (تعديل جديد)
+mkdir -p /mnt/datadisk/docker
 cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
-  "storage-driver": "overlay2"
+  "storage-driver": "overlay2",
+  "data-root": "/mnt/datadisk/docker"
 }
 EOF
 mkdir -p /etc/systemd/system/docker.service.d
@@ -71,17 +86,20 @@ systemctl daemon-reload
 systemctl restart docker
 systemctl enable docker
 
+# 4. إضافة اليوزر devsecops لجروب الدوكر (تعديل جديد)
+usermod -aG docker devsecops
 
 echo ".........----------------#################._.-.-Java and MAVEN-.-._.#################----------------........."
-apt install openjdk-11-jdk maven -y
+apt install openjdk-17-jdk maven -y
 java -version
 mvn -v
 
 echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-echo 'deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/' > /etc/apt/sources.list.d/jenkins.list
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | tee /usr/share/keyrings/jenkins.asc > /dev/null
+echo 'deb [signed-by=/usr/share/keyrings/jenkins.asc] http://pkg.jenkins.io/debian-stable binary/' > /etc/apt/sources.list.d/jenkins.list
 apt update
 apt install -y jenkins
+
 systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
